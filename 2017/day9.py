604c70eff84f9df36021f9be751c4c3a3edcd227
Alexandre Cunha <alexandre.cunha@gmail.com>
2017-12-14 00:40:37 -0600

    Solved day9

---
+++ 2017/day9.py
@@ -0,0 +1,55 @@
+def traverse(input, cache={}):
+    stack = list()
+    score = 0
+    for pos in range(len(input)):
+        c = input[pos]
+        if len(stack) > 0 and (stack[-1] == '<' or stack[-1] == '!'):  # trashing
+            if c == '>' or stack[-1] == '!':
+                stack.pop()
+            elif c == '!':
+                stack.append(c)
+            else:
+                cache['garbage'] = cache.get('garbage', 0) + 1
+        else:
+            if c == '{' or c == '<' or c == '!':
+                stack.append(c)
+                if c == '{':
+                    score += 1
+            elif c == '}':
+                stack.pop()
+                yield score
+                score -= 1
+
+
+def part1(input):
+    return sum(traverse(input))
+
+
+def part2(input):
+    cache = {}
+    list(traverse(input, cache))
+    return cache.get('garbage', 0)
+
+
+if __name__ == '__main__':
+    assert(part1('{}') == 1)
+    assert(part1('{{{}}}') == 6)
+    assert(part1('{{},{}}') == 5)
+    assert(part1('{{{},{},{{}}}}') == 16)
+    assert(part1('{<a>,<a>,<a>,<a>}') == 1)
+    assert(part1('{{<ab>},{<ab>},{<ab>},{<ab>}}') == 9)
+    assert(part1('{{<!!>},{<!!>},{<!!>},{<!!>}}') == 9)
+    assert(part1('{{<a!>},{<a!>},{<a!>},{<ab>}}') == 3)
+
+    with open('day9.in', 'r') as fh:
+        data = fh.read().strip()
+    print(part1(data))
+
+    assert(part2('<>') == 0)
+    assert(part2('<random characters>') == 17)
+    assert(part2('<<<<>') == 3)
+    assert(part2('<{!>}>') == 2)
+    assert(part2('<!!>') == 0)
+    assert(part2('<!!!>>') == 0)
+    assert(part2('<{o"i!a,<{i<a>') == 10)
+    print(part2(data))
