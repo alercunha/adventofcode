5209be940c3f258cf236ea5cce22bba48775537c
Alex Cunha <ale@rcunha.net>
2018-04-17 20:46:22 -0500

    Solved day16 part 2 (a bit slow)

--- 2017/day16.py
+++ 2017/day16.py
@@ -1,38 +1,46 @@
-def nav(array, pos):
-    size = len(array)
-    for i in range(size):
-        yield array[(i + pos) % size]
+def compile(size, commands):
+    idxs = [i for i in range(size)]
+    for comm in commands:
+        if comm[0] == 's':
+            idxs = [idxs[(i + size - comm[1]) % size] for i in range(size)]
+        elif comm[0] == 'x':
+            idxs[comm[1]], idxs[comm[2]] = idxs[comm[2]], idxs[comm[1]]
+    return 'r', idxs
 
 
 def dance(size, input, rounds):
+    memory = {}
     array = list('abcdefghijklmnopqrstuvwxyz'[0:size])
-    pos = 0
 
     commands = []
+    mini_dance = []
     for comm in [i.strip() for i in input.split(',')]:
         if comm[0] == 's':
-            commands.append(('s', int(comm[1:])))
+            mini_dance.append(('s', int(comm[1:])))
         elif comm[0] == 'x':
             va, vb = comm[1:].split('/')
-            commands.append(('x', int(va), int(vb)))
+            mini_dance.append(('x', int(va), int(vb)))
         elif comm[0] == 'p':
+            commands.append(compile(size, mini_dance))
+            mini_dance = []
             va, vb = comm[1:].split('/')
             commands.append(('p', va, vb))
 
     while rounds > 0:
-        for comm in commands:
-            if comm[0] == 's':
-                pos = (pos - comm[1])
-                pos = size - (-pos % size)
-            elif comm[0] == 'x':
-                a, b = (comm[1] + pos) % size, (comm[2] + pos) % size
-                array[a], array[b] = array[b], array[a]
-            elif comm[0] == 'p':
-                a, b = array.index(comm[1]), array.index(comm[2])
-                array[a], array[b] = array[b], array[a]
+        input = ''.join(array)
+        if input in memory:
+            array = memory[input]
+        else:
+            for comm in commands:
+                if comm[0] == 'p':
+                    a, b = array.index(comm[1]), array.index(comm[2])
+                    array[a], array[b] = array[b], array[a]
+                elif comm[0] == 'r':
+                    array = [array[i] for i in comm[1]]
+            memory[input] = array
         rounds -= 1
 
-    return ''.join(nav(array, pos))
+    return ''.join(array)
 
 
 def part1(size, input):
@@ -51,4 +59,4 @@ if __name__ == '__main__':
     print(part1(16, data))
 
     assert(part2(5, 's1, x3/4, pe/b', 2) == 'ceadb')
-    #print(part2(16, data, 1 * 1000 * 1000 * 1000))
+    print(part2(16, data, 1 * 1000 * 1000 * 1000))
