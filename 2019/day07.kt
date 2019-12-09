680c8dda7813b2b5c8758b88f3b8b15c5cffc2a2
Ale Cunha <ale@rcunha.net>
2019-12-08 23:10:40 -0600

    Solved day07 part 2

--- 2019/day07.kt
+++ 2019/day07.kt
@@ -1,34 +1,52 @@
 import java.io.File
-import day05lib.intCode
+import day05lib.Program
 import day05lib.split
 
 fun thruster(program: String, size: Int, input: Int, phases: List<Int>): Int {
-	var signal = input
+	var amps = ArrayList<Program>()
 	(0..size-1).forEach{
-		var out = intCode(split(program), listOf(phases[it], signal))[0]
-		signal = out
+		var p = Program(split(program))
+		p.run(listOf(phases[it]))
+		amps.add(p)
 	}
-	return signal	
+	var signal = input
+	var pos = 0
+	var halted = false
+	while (!halted) {
+		var result = amps[pos].run(listOf(signal))
+		if (pos == size - 1 && result.second)
+			halted = true
+		signal = result.first.last()
+		pos = (pos + 1) % size
+	}
+	return signal
 }
 
-var combination = sequence {
-	(0..4).forEach{i0 -> 
-		(0..4).forEach{i1 -> 
-			(0..4).forEach{i2 -> 
-				(0..4).forEach{i3 -> 
-					(0..4).forEach{i4 -> 
-						var all = setOf(i0, i1, i2, i3, i4)
-						if (all.size == 5)
-							yield(listOf(i0, i1, i2, i3, i4))
+fun combine(start: Int, end: Int): Sequence<List<Int>> {
+	var seq = sequence {
+		(start..end).forEach{i0 -> 
+			(start..end).forEach{i1 -> 
+				(start..end).forEach{i2 -> 
+					(start..end).forEach{i3 -> 
+						(start..end).forEach{i4 -> 
+							var all = setOf(i0, i1, i2, i3, i4)
+							if (all.size == 5)
+								yield(listOf(i0, i1, i2, i3, i4))
+						}
 					}
 				}
 			}
 		}
 	}
+	return seq
 }
 
 fun part1(program: String): Int? {
-	return combination.map{ thruster(program, 5, 0, it) }.max()
+	return combine(0, 4).map{ thruster(program, 5, 0, it) }.max()
+}
+
+fun part2(program: String): Int? {
+	return combine(5, 9).map{ thruster(program, 5, 0, it) }.max()
 }
 
 fun main() {
@@ -39,4 +57,10 @@ fun main() {
 
     var lines = File("day07.in").readLines()
     println(part1(lines[0]))
+
+    assert(thruster("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5", 5, 0, listOf(9,8,7,6,5)) == 139629729)
+    assert(part2("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5") == 139629729)
+    assert(part2("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10") == 18216)
+
+    println(part2(lines[0]))
 }
