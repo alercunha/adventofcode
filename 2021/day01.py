08b8ed0607bbc1be71879492d5fd4e75d513238f
Ale Cunha <ale@rcunha.net>
2021-12-01 22:31:51 -0800

    Day 01 part 2

--- 2021/day01.py
+++ 2021/day01.py
@@ -7,8 +7,18 @@ def part1(input):
     return sum(1 if data[i] > data[i - 1] else 0 for i in range(1, len(data)))
 
 
+def part2(input):
+    data = parse_input(input)
+
+    def measure(i):
+        return data[i] + data[i + 1] + data[i + 2]
+
+    count = [1 if measure(i) > measure(i - 1) else 0 for i in range(1, len(data) - 2)]
+    return sum(count)
+
+
 if __name__ == '__main__':
-    example1_input = """
+    example_input = """
     199
     200
     208
@@ -20,7 +30,10 @@ if __name__ == '__main__':
     260
     263
     """
-    assert part1(example1_input) == 7
+    assert part1(example_input) == 7
     with open('day01.in', 'r') as fh:
         day01_input = fh.read()
     print(part1(day01_input))
+
+    assert part2(example_input) == 5
+    print(part2(day01_input))
